1f12b239a7bbae7aa5a58e15e78693da596ee31f
Michael Boltachev <boltachev918@gmail.com>
2024-03-07 21:16:10 +0300

    MAIN

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello')
+import random
+from config import load_config
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart, ChatMemberUpdatedFilter, BaseFilter, KICKED, MEMBER
+from aiogram.types import Message, ChatMemberUpdated
+
+config = load_config()
+BOT_TOKEN = config.telegram_bot.token
+ADMINS = config.telegram_bot.admin_ids
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
