34f74ee69b777ca5c4dbe43fe2cc9525ec093fb3
Michael Boltachev <boltachev918@gmail.com>
2024-03-09 08:21:24 +0300

    new main

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-import random
-from config import load_config
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart, ChatMemberUpdatedFilter, BaseFilter, KICKED, MEMBER
-from aiogram.types import Message, ChatMemberUpdated
+import asyncio
+import logging
+from config import load_config, Config
+from aiogram import Bot, Dispatcher
+from handlers import other_handlers, user_handlers
 
-config = load_config()
-BOT_TOKEN = config.telegram_bot.token
-ADMINS = config.telegram_bot.admin_ids
+logger = logging.getLogger(__name__)
 
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher()
+
+async def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    config: Config = load_config()
+
+    bot: str = Bot(token=config.telegram_bot.token, parse_mode='HTML')
+    dp = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
