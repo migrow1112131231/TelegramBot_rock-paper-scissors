e2d5e21b68ef90b9a436440c21681a63b02a2863
Michael Boltachev <boltachev918@gmail.com>
2024-03-10 15:18:18 +0300

    lexicon

--- lexicon/__init__.py
+++ lexicon/__init__.py
@@ -1,1 +1,2 @@
 from .lexicon import LEXICON_RU
+from .lexicon_for_menu import LEXICON_MENU
---
+++ lexicon/lexicon_for_menu.py
@@ -0,0 +1,6 @@
+LEXICON_MENU: 'dict[str, str | list[str]]' = {
+    '/start': 'Старт',
+    '/help': 'Правила',
+    '/contacts': 'Контактная информация',
+    '/info': 'О боте'
+}
