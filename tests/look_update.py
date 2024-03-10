fede0c1c679f5271e54c8d1e45f6d7d0ca506fd9
Michael Boltachev <boltachev918@gmail.com>
2024-03-10 15:20:08 +0300

    test

--- tests/look_update.py
+++ tests/look_update.py
@@ -1,19 +1,16 @@
-import random
-from environs import Env
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart, ChatMemberUpdatedFilter, BaseFilter, KICKED, MEMBER
-from aiogram.types import Message, ChatMemberUpdated
+from config import add_config, Config
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env('BOT_TOKEN')
+config: Config = add_config()
+BOT_TOKEN = config.telegram_bot.token
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
 
 @dp.message()
 async def look_message(message: Message):
     print(message.model_dump_json(indent=4, exclude_none=True))
 
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    dp.start_polling(bot)
